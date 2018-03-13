3e6fe29fcac0eee327476b7351a59f61d0ebd890
Molly Wojcik <mollywojcik@gmail.com>
2018-03-12 18:19:25 -0600

    updated port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var app = express();
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 var app = express();
 
